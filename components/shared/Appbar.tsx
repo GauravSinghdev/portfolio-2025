cc7e8ed8fba1d37f7788a81ab099bba95834440f
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-08-08 01:38:39 +0530

    fixing appbar.tsx

--- components/shared/Appbar.tsx
+++ components/shared/Appbar.tsx
@@ -55,10 +55,10 @@ export default function Appbar() {
       {isMenuOpen && (
         <div className="sm:hidden bg-white dark:bg-gray-800 border-t border-gray-200">
           <div className="flex flex-col items-start gap-4 p-4 text-gray-700 dark:text-gray-200 bg-transparent">
-            <Link href={"/#projects"} onClick={toggleMenu}>
+            <Link className=" w-full p-1" href={"/#projects"} onClick={toggleMenu}>
               Projects
             </Link>
-            <Link href={"/reviews"} onClick={toggleMenu}>
+            <Link className=" w-full p-1" href={"/reviews"} onClick={toggleMenu}>
               Reviews
             </Link>
           </div>
